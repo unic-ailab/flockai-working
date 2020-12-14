fc1a762d8a97a9fe367a706f001c5272e9b6cb38
kosnet2 <avogian.karlen@hotmail.com>
2020-12-14 19:22:11 +0200

    feat(): Changed controller

--- simulation/controllers/mavic2dji/mavic2dji.py
+++ simulation/controllers/mavic2dji/mavic2dji.py
@@ -1,5 +1,36 @@
 from flockai.webots_controllers.mavic2dji import KeyboardMavic2DJI
+from flockai.models.devices.device_enums import EnableableDevice, NonEnableableDevice, MotorDevice, AircraftAxis, \
+    Relative2DPosition
+
+enableable_devices = [
+
+    (EnableableDevice.RECEIVER, "receiver"),
+    (EnableableDevice.CAMERA, "camera"),
+    (EnableableDevice.KEYBOARD, None),
+    (EnableableDevice.BATTERY_SENSOR, None),
+    (EnableableDevice.INERTIAL_UNIT, "inertial unit"),
+    (EnableableDevice.GPS, "gps"),
+    (EnableableDevice.COMPASS, "compass"),
+    (EnableableDevice.GYRO, "gyro")
+]
+
+non_enableable_devices = [
+    (NonEnableableDevice.EMITTER, "emitter"),
+    (NonEnableableDevice.LED, "front left led"),
+    (NonEnableableDevice.LED, "front right led"),
+]
+
+motor_devices = [
+    (MotorDevice.CAMERA, "camera roll", AircraftAxis.ROLL),
+    (MotorDevice.CAMERA, "camera pitch", AircraftAxis.PITCH),
+    (MotorDevice.CAMERA, "camera yaw", AircraftAxis.YAW),
+    (MotorDevice.PROPELLER, "front left propeller", Relative2DPosition(1, -1)),
+    (MotorDevice.PROPELLER, "front right propeller", Relative2DPosition(1, 1)),
+    (MotorDevice.PROPELLER, "rear left propeller", Relative2DPosition(-1, -1)),
+    (MotorDevice.PROPELLER, "rear right propeller", Relative2DPosition(-1, 1)),
+]
 
 # Create drone instance and run its controller
-controller = KeyboardMavic2DJI()
+controller = KeyboardMavic2DJI(enableable_devices, non_enableable_devices, motor_devices)
+
 controller.run()
